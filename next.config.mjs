781405b5f3dd090e89016d9ea1c12bdef309edfc
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-10-19 13:02:37 -0600

    Unoptimized Next.js images

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/web",
-  output: "export",
-  reactStrictMode: true,
+    basePath: "/web",
+    output: "export",
+    reactStrictMode: true,
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
