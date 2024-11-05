285e4c51a8041484e876ef7e98fc15f3919d7235
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-11-05 16:45:38 -0600

    Fix default device's theme mode (#8)
    - Implemented CSS distinction between dark and white logos
    - Fixed routing of `/` to be redirected automatically to `/web` endpoint
    - Changed webpage meta description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import logo from "../assets/logo/rebo-white.png"
+import logoWhite from "../assets/logo/rebo-white.png";
+import logoBlack from "../assets/logo/rebo-black.png";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image src={logo} alt="logo" />
+        <Image src={logoWhite} alt="logo" className="logo-white" />
+        <Image src={logoBlack} alt="logo" className="logo-black" />
         Lo sentimos, este sitio web se encuentra bajo<br/> 
         construcción. Comunícate con nosotros en<br/>
         los siguientes enlaces:
