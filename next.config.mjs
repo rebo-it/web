03e555ec582f7495c4b3836f050a0d3ed3cd9675
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2025-01-12 21:50:48 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/web",
     output: "export",
     reactStrictMode: true,
     images: {
         unoptimized: true
-    },
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/web",
-                basePath: false,
-                permanent: false,
-            },
-        ];
     }
 };
 
